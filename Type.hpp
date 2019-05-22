c1451b1bb062a6dc88a2a49809c8aa010b84de7c
David Jaquet <djaquet5@gmail.com>
2019-05-22 15:59:51 +0200

    filled the enum

--- Type.hpp
+++ Type.hpp
@@ -4,6 +4,22 @@
 
 #pragma once
 
-enum Type {
+class Type {
+public:
+    Type(const Type&) = delete;
+    Type& operator = (const Type&) = delete;
 
+    static const Type thief, son, girl, policeman, father, mother;
+
+private:
+    Type(const char *name) : name(name){};
+
+    const char *name;
 };
+
+const Type Type::thief("Thief");
+const Type Type::son("Son");
+const Type Type::girl("Girl");
+const Type Type::policeman("Policeman");
+const Type Type::father("Father");
+const Type Type::mother("Mother");
